ee03322891aa76b3b60f54ae64f3ca4f28bcd4c1
AndreaDeBlasio55 <39781342+AndreaDeBlasio95@users.noreply.github.com>
2024-03-08 17:53:08 +0100

    added header and user inputs ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,7 @@
 export default function Header() {
-  return <header>React Investment Calculator</header>;
+  return (
+    <header className="header">
+      <h1 className="header-title">React Investment Calculator</h1>
+    </header>
+  );
 }
